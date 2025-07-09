cb50805758fcffdc73c6807daca608a0fbbfce99
Ludwin Blanco <lblancoorellana@gmail.com>
2025-07-09 02:25:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -60,14 +60,12 @@ export default function AppLayout({
             <SidebarMenu>
                 {menuItems.map((item) => (
                     <SidebarMenuItem key={item.href}>
-                        <Link href={item.href} passHref legacyBehavior>
-                            <SidebarMenuButton asChild isActive={pathname === item.href} tooltip={{children: item.label, side: 'right'}}>
-                                <a>
-                                    <item.icon />
-                                    <span>{item.label}</span>
-                                </a>
-                            </SidebarMenuButton>
-                        </Link>
+                        <SidebarMenuButton asChild isActive={pathname === item.href} tooltip={{children: item.label, side: 'right'}}>
+                            <Link href={item.href}>
+                                <item.icon />
+                                <span>{item.label}</span>
+                            </Link>
+                        </SidebarMenuButton>
                     </SidebarMenuItem>
                 ))}
             </SidebarMenu>
